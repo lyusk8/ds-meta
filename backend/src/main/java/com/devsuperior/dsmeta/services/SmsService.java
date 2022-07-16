ac5fcb3ce8ac2eda75240335c150f39708a10eec
lyusk8 <lyusk8@gmail.com>
2022-07-16 09:30:26 -0300

    Twilio sms

---
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -0,0 +1,47 @@
+package com.devsuperior.dsmeta.services;
+
+import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.repositories.SaleRepository;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class SmsService {
+
+    @Value("${twilio.sid}")
+    private String twilioSid;
+
+    @Value("${twilio.key}")
+    private String twilioKey;
+
+    @Value("${twilio.phone.from}")
+    private String twilioPhoneFrom;
+
+    @Value("${twilio.phone.to}")
+    private String twilioPhoneTo;
+
+    @Autowired
+    private SaleRepository repository;
+
+    public void sendSms(Long saleId) {
+
+        Sale sale = repository.findById(saleId).get();
+
+        String msg = "Vendedor " + sale.getSellerName() + " Vendeu "+sale.getAmount()+" no dia "+sale.getDate();
+
+        Twilio.init(twilioSid, twilioKey);
+
+        PhoneNumber to = new PhoneNumber(twilioPhoneTo);
+        PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
+
+        Message message = Message.creator(to, from, msg).create();
+
+        System.out.println(message.getSid());
+    }
+}
