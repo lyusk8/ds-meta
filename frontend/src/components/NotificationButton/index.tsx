63054e9a3e2c191896f5be4df04dc9635183619b
lyusk8 <lyusk8@gmail.com>
2022-07-17 12:36:01 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import botao from '../../assets/img/notification-icon.svg';
 import { BASE_URL } from '../../utils/requests';
+import {toast} from 'react-toastify';
 import './styles.css';
 
 type props = {
@@ -11,7 +12,7 @@ export default function NotificationButton({saleId} : props){
   function sendMessage(id : number){
     axios(`${BASE_URL}/sales/${id}/notification`)
     .then(response =>{
-      console.log("SUCESSO!!")
+     toast.info("SMS enviada com sucesso.")
     })
   }
   return(
